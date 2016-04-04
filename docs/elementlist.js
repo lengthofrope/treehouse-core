02d56aa74615aac093cb585bea8c69e3f5a89154
Bas de Kort <bdekort@gmail.com>
2016-04-04 09:12:50 +0200

    Add: documentation

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","LengthOfRope\\Treehouse\\Template\\MetaBox"],["c","LengthOfRope\\Treehouse\\Template\\Services\\Translation"],["c","LengthOfRope\\Treehouse\\Template\\Template"]];
