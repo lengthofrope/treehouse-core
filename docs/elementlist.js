94d33566e96a1e3f84f497e18004efb8f04f526d
Bas de Kort <bdekort@gmail.com>
2016-04-05 17:08:43 +0200

    Update: documentation

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","LengthOfRope\\Treehouse\\Template\\MetaBox"],["c","LengthOfRope\\Treehouse\\Template\\Services\\Translation"],["c","LengthOfRope\\Treehouse\\Template\\Template"]];
+ApiGen.elements = [["c","LengthOfRope\\Treehouse\\Core"],["c","LengthOfRope\\Treehouse\\Template\\MetaBox"],["c","LengthOfRope\\Treehouse\\Template\\Services\\Translation"],["c","LengthOfRope\\Treehouse\\Template\\Template"],["c","LengthOfRope\\Treehouse\\Utils\\PluginLoadPriority"]];
