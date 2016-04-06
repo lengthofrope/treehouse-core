cd60e99148dfc3dfd1e256a0070df6ebdb302f7b
Bas de Kort <bdekort@gmail.com>
2016-04-06 08:58:07 +0200

    Add: A component class which will load serveral things later on and to which all plugins and themes can extend to.

---
+++ lib/Component.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LengthOfRope\Treehouse;
+
+/**
+ * The base class all plugin and themes should extend to.
+ *
+ * @author LengthOfRope, Bas de Kort <bdekort@gmail.com>
+ */
+abstract class Component
+{
+    public function __construct() {
+        
+    }
+}
--- lib/Core.php
+++ lib/Core.php
@@ -8,11 +8,13 @@ namespace LengthOfRope\Treehouse;
  *
  * @author LengthOfRope, Bas de Kort <bdekort@gmail.com>
  */
-class Core
+class Core extends Component
 {
     public function __construct()
     {
         // Make sure Core is loaded before any other Treehouse plugin
         new Utils\PluginLoadPriority(TH_CORE_FILE);
+
+        parent::__construct();
     }
 }
